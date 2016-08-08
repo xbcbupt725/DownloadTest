359ede754703d73389a1fc9efd1f07a7e5fae610
xbcbupt725 <xbcbupt725@gmail.com>
2016-08-08 13:13:57 +0800

    first commit

---
+++ src/com/example/test/MainActivity.java
@@ -0,0 +1,116 @@
+package com.example.test;
+
+import android.os.Bundle;
+import android.os.Environment;
+import android.support.v4.app.FragmentActivity;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import com.example.downloadtest.R;
+import com.example.test.DownloadUtil.OnDownloadListener;
+
+public class MainActivity extends FragmentActivity {
+
+	private static final String TAG = MainActivity.class.getSimpleName();
+
+	private ProgressBar mProgressBar;
+	private boolean isDelete;
+	private Button start;
+	private Button pause;
+	private Button delete;
+	private Button reset;
+	private TextView total;
+
+	private int max;
+
+	private DownloadUtil mDownloadUtil;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		mProgressBar = (ProgressBar) findViewById(R.id.progressBar1);
+		start = (Button) findViewById(R.id.button_start);
+		pause = (Button) findViewById(R.id.button_pause);
+		delete = (Button) findViewById(R.id.button_delete);
+		reset = (Button) findViewById(R.id.button_reset);
+		total = (TextView) findViewById(R.id.textView_total);
+		String urlString = "http://bbra.cn/Uploadfiles/imgs/20110303/fengjin/013.jpg";
+		String localPath = Environment.getExternalStorageDirectory()
+				.getAbsolutePath() + "/local";
+		mDownloadUtil = new DownloadUtil(2, localPath, "abc.jpg", urlString,
+				this);
+		mDownloadUtil.setOnDownloadListener(new OnDownloadListener() {
+
+			@Override
+			public void downloadStart(int fileSize) {
+				// TODO Auto-generated method stub
+				Log.w(TAG, "fileSize::" + fileSize);
+				max = fileSize;
+				mProgressBar.setMax(fileSize);
+			}
+
+			@Override
+			public void downloadProgress(int downloadedSize) {
+				// TODO Auto-generated method stub
+				Log.w(TAG, "Compelete::" + downloadedSize);
+				if(isDelete){
+					isDelete = false;
+					mProgressBar.setProgress(0);
+					total.setText("0%");
+				}else{
+					mProgressBar.setProgress(downloadedSize);
+					total.setText((int) downloadedSize * 100 / max + "%");
+				}
+				
+			}
+
+			@Override
+			public void downloadEnd() {
+				// TODO Auto-generated method stub
+				Log.w(TAG, "ENd");
+			}
+		});
+		start.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View arg0) {
+				// TODO Auto-generated method stub
+				mDownloadUtil.start();
+			}
+		});
+		pause.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View arg0) {
+				// TODO Auto-generated method stub
+				mDownloadUtil.pause();
+			}
+		});
+		delete.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View arg0) {
+				// TODO Auto-generated method stub
+			//	mDownloadUtil.pause();
+				
+				mDownloadUtil.delete();
+				isDelete = true;
+			
+			}
+		});
+		reset.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View arg0) {
+				// TODO Auto-generated method stub
+				mDownloadUtil.reset();
+			}
+		});
+	}
+
+}
